ce4e17e5b5f40be5dbb31d931ac2f708d677ee24
gptcafe <122703358+gptcafe@users.noreply.github.com>
2024-06-28 18:23:19 -0400

    changed logo and powered by

--- i18n/lang/app.en.ts
+++ i18n/lang/app.en.ts
@@ -1,6 +1,6 @@
 const translation = {
   common: {
-    welcome: 'Welcome to use',
+    welcome: '',
     appUnavailable: 'App is unavailable',
     appUnkonwError: 'App is unavailable',
   },
@@ -8,25 +8,25 @@ const translation = {
     newChat: 'New chat',
     newChatDefaultName: 'New conversation',
     openingStatementTitle: 'Opening statement',
-    powerBy: 'Powered by',
+    powerBy: '',
     prompt: 'Prompt',
     privatePromptConfigTitle: 'Conversation settings',
     publicPromptConfigTitle: 'Initial Prompt',
-    configStatusDes: 'Before start, you can modify conversation settings',
+    configStatusDes: 'Modify conversation settings before you start',
     configDisabled:
       'Previous session settings have been used for this session.',
     startChat: 'Start Chat',
     privacyPolicyLeft:
       'Please read the ',
     privacyPolicyMiddle:
-      'privacy policy',
+      'BuildGPT',
     privacyPolicyRight:
-      ' provided by the app developer.',
+      ' privacy policy.',
   },
   errorMessage: {
-    valueOfVarRequired: 'Variables value can not be empty',
+    valueOfVarRequired: 'Variable value can not be empty',
     waitForResponse:
-      'Please wait for the response to the previous message to complete.',
+      'Please wait for the response to the previous message.',
   },
 }
 
